b226577a5614f574825d430f8937ad7f9cacf57f
DhanasaiBontu <bdhanasaichowdary@gmail.com>
2026-01-03 22:00:18 +0530

    UC1: Check employee attendance

--- EmployeeWageComputation/src/EmployeeWageComputation.java
+++ EmployeeWageComputation/src/EmployeeWageComputation.java
@@ -1,3 +1,14 @@
+import java.util.Random;
+
 public class EmployeeWageComputation {
 
+    public void checkAttendance() {
+        int empCheck = new Random().nextInt(2);
+
+        if (empCheck == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
 }
--- EmployeeWageComputation/src/Main.java
+++ EmployeeWageComputation/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        EmployeeWageComputation emp=new EmployeeWageComputation();
+
+        EmployeeWageComputation emp = new EmployeeWageComputation();
+        emp.checkAttendance();
     }
 }
